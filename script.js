3abf2914c322a08f26a5d4fd6b92793082dbeda1
Arpit Sharma <84865895+arpitsharma12@users.noreply.github.com>
2022-05-11 21:10:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ addbtn.addEventListener(`click`, function () {
   newtaskpop.classList.remove(`hidden`);
   noTaskErrorPopup.textContent = ``;
   taskinput.value="";
-  location.reload();
 });
+location.reload();
 
 cancelbtn.addEventListener(`click`, function () {
   newtaskpop.classList.add(`hidden`);
